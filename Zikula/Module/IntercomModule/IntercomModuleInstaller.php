e27f2b6f94c5ad9344a3cad937eed57df3535ed8
Kaik <Kaik@kaikacer2>
2014-12-07 22:44:53 +0100

    Fix install and remove. Module install and uninstall check

--- Zikula/Module/IntercomModule/IntercomModuleInstaller.php
+++ Zikula/Module/IntercomModule/IntercomModuleInstaller.php
@@ -18,44 +18,49 @@
 namespace Zikula\Module\IntercomModule;
 
 use DoctrineHelper;
-use EventUtil;
 use System;
+use ZLanguage;
 use Zikula\Module\IntercomModule\Entity\MessageEntity;
 
 
 class IntercomModuleInstaller extends \Zikula_AbstractInstaller
 {
 
-    public function SetDefaultVars()
-    { 
-        $this->setVar('messages_limitarchive', '50');
-        $this->setVar('messages_limitoutbox', '50');
-        $this->setVar('messages_limitinbox', '50');
-        $this->setVar('messages_allowhtml', false);
-        $this->setVar('messages_allowsmilies', false);
-        $this->setVar('messages_perpage', '25');
+    const MODULENAME = 'ZikulaIntercomModule';    
+    
+    /**
+     * get the default module var values
+     *
+     * @return array
+     */
+    public static function getDefaultVars()
+    {
+        $dom = ZLanguage::getModuleDomain(self::MODULENAME);
 
-        $this->setVar('messages_allow_emailnotification', true);
-        $this->setVar('messages_mailsubject', $this->__('You have a new private message'));
-        $this->setVar('messages_fromname', '');
-        $this->setVar('messages_from_email', '');
-
-        $this->setVar('messages_allow_autoreply', true);
-
-        $this->setVar('messages_userprompt', $this->__('Welcome to the private messaging system'));
-        $this->setVar('messages_userprompt_display', false);
-        $this->setVar('messages_active', true);
-        $this->setVar('messages_maintain', $this->__('Sorry! The private messaging system is currently off-line for maintenance. Please check again later, or contact the site administrator.'));
-
-        $this->setVar('messages_protection_on', true);
-        $this->setVar('messages_protection_time', '15');
-        $this->setVar('messages_protection_amount', '15');
-        $this->setVar('messages_protection_mail', false);
-
-        $this->setVar('messages_welcomemessagesender', $this->__('admin'));
-        $this->setVar('messages_welcomemessagesubject', $this->__('Welcome to the private messaging system on %sitename%'));  // quotes are important here!!
-        $this->setVar('messages_welcomemessage', $this->__('Hello!' .'Welcome to the private messaging system on %sitename%. Please remember that use of the private messaging system is subject to the site\'s terms of use and privacy statement. If you have any questions or encounter any problems, please contact the site administrator. Site admin')); // quotes are important here!!!
-        $this->setVar('messages_savewelcomemessage', false);
+        return array(
+            'messages_limitarchive'=> '50',
+            'messages_limitoutbox'=> '50',
+            'messages_limitinbox'=> '50',
+            'messages_allowhtml'=> false,
+            'messages_allowsmilies'=> false,
+            'messages_perpage'=> '25',
+            'messages_allow_emailnotification'=> true,
+            'messages_mailsubject'=> __('You have a new private message', $dom),
+            'messages_fromname'=> '',
+            'messages_from_email'=> '',
+            'messages_allow_autoreply'=> true,
+            'messages_userprompt'=> __('Welcome to the private messaging system', $dom),
+            'messages_userprompt_display'=> false,
+            'messages_active'=> true,
+            'messages_maintain'=> __('Sorry! The private messaging system is currently off-line for maintenance. Please check again later, or contact the site administrator.', $dom),
+            'messages_protection_on'=> true,
+            'messages_protection_time'=> '15',
+            'messages_protection_amount'=> '15',
+            'messages_protection_mail'=> false,
+            'messages_welcomemessagesender'=> __('admin', $dom),
+            'messages_welcomemessagesubject'=> __('Welcome to the private messaging system on %sitename%', $dom),  // quotes are important here!!
+            'messages_welcomemessage'=> __('Hello!' .'Welcome to the private messaging system on %sitename%. Please remember that use of the private messaging system is subject to the site\'s terms of use and privacy statement. If you have any questions or encounter any problems, please contact the site administrator. Site admin', $dom), // quotes are important here!!!
+            'messages_savewelcomemessage'=> false);
     
     }   
     
@@ -68,10 +73,7 @@ class IntercomModuleInstaller extends \Zikula_AbstractInstaller
             $this->request->getSession()->getFlashBag()->add('error', $e->getMessage());
             return false;
         }
-        $this->SetDefaultVars();
-
-        EventUtil::registerPersistentModuleHandler('InterCom', 'user.account.create',
-                array('InterCom_Listener_CreateUserListener', 'onCreateUser'));
+        $this->setVars(self::getDefaultVars());
 
         return true;
     }
@@ -95,8 +97,6 @@ class IntercomModuleInstaller extends \Zikula_AbstractInstaller
         }
         // Delete any module variables
         $this->delVars();
-        
-        EventUtil::unregisterPersistentModuleHandlers('InterCom');
         return true;
     }
 }
