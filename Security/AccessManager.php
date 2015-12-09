41f0793c6fb161afb94b2d0c41e58e8921b73fdb
Kaik <kapitanogor@googlemail.com>
2015-12-09 15:36:28 +0100

    Move access to service AccessManager

--- Security/AccessManager.php
+++ Security/AccessManager.php
@@ -10,13 +10,9 @@
  * Please see the CREDITS.txt file distributed with this source code for further
  * information regarding copyright.
  */
-namespace Zikula\IntercomModule\Util;
+namespace Zikula\IntercomModule\Security;
 
-use UserUtil;
-use SecurityUtil;
-use ModUtil;
-
-class Access {
+class AccessManager {
    
     /***
      * Do all user checks in one method:
@@ -26,30 +22,32 @@ class Access {
      * Returns false if use has permissions.
      * On exit, $uid has the user's UID if logged in.
     */  
-
-    public static function checkAccess($access = ACCESS_READ)
+    public function hasPermission($access = ACCESS_READ)
     {
-
         // If not logged in, redirect to login screen
-        if (!UserUtil::isLoggedIn())
-	{
+        if (!\UserUtil::isLoggedIn())
+		{
             return false;
-	}
+		}
         // Perform access check
-        if (!SecurityUtil::checkPermission('ZikulaIntercomModule::', '::', $access))
+        if (!$this->hasPermissionRaw('ZikulaIntercomModule::', '::', $access))
         {
             return false;
         }
         // Maintenance message
-        if (ModUtil::getVar('ZikulaIntercomModule','active') == 0 && !SecurityUtil::checkPermission('ZikulaIntercomModule::', '::', ACCESS_ADMIN)) {
+        if (\ModUtil::getVar('ZikulaIntercomModule','active') == 0 && !\SecurityUtil::checkPermission('ZikulaIntercomModule::', '::', ACCESS_ADMIN)) {
             return false;
         }
 
         // Get the uid of the user
-        $uid = UserUtil::getVar('uid');
+        $uid = \UserUtil::getVar('uid');
 
         // Return user uid to signify everything is OK.
         return $uid;
-    } 
-    
+    }
+
+    public function hasPermissionRaw($component, $instance, $level)
+    {
+        return \SecurityUtil::checkPermission($component, $instance, $level);
+    }       
 }
